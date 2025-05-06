4cb441267226c9d94b5356f95da1ee71d6707732
saksham gupta <guptasaksham2510@gmail.com>
2025-05-06 19:39:42 +0530

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,8 +47,45 @@ class ModelTrainer:
                 "CatBoostRegressor":CatBoostRegressor(verbose=False),
                 "AdaBoostRegressor":AdaBoostRegressor()
             }
-            
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params = {
+    "LinearRegression": {},
+
+    "KNeighborsRegressor": {
+        'n_neighbors': [3, 5, 7, 9]
+    },
+
+    "DecisionTreeRegressor": {
+        'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+    },
+
+    "RandomForestRegressor": {
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+
+    "GradientBoostingRegressor": {
+        'learning_rate': [.1, .01, .05, .001],
+        'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+
+    "XGBRegressor": {
+        'learning_rate': [.1, .01, .05, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+
+    "CatBoostRegressor": {
+        'depth': [6, 8, 10],
+        'learning_rate': [0.01, 0.05, 0.1],
+        'iterations': [30, 50, 100]
+    },
+
+    "AdaBoostRegressor": {
+        'learning_rate': [.1, .01, 0.5, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    }
+}
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
             
             # to get best model score from dict
             best_model_score=max(sorted(model_report.values()))
